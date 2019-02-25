134df4e9a110c97931c78652aa69dbb73d88b3d3
aminasamina <47269627+aminasamina@users.noreply.github.com>
2019-02-25 20:50:23 +0200

    Статистика - изпитни задачи

---
+++ Статистика - изпитни задачи.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Статистика
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int agents = int.Parse(Console.ReadLine());
+            int[] agentsOffers = new int[agents];
+            decimal[] values = new decimal[agents];
+            for (int i = 0; i < agents; i++)
+            {
+                agentsOffers[i] = int.Parse(Console.ReadLine());
+                values[i] = 0;
+                for (int offer  = 0; offer  < agentsOffers[i]; offer ++)
+                {
+                    int area = int.Parse(Console.ReadLine());
+                    decimal price = decimal.Parse(Console.ReadLine());
+                    values[i] += area * price;
+                }
+            }
+            Console.WriteLine(Math.Round(values.Average(),3));
+        }
+    }
+}
